e99f71e3e79ea7147b69acb6e33790d868cf2c5d
Ludwig <workingpeter@wp.pl>
2021-11-19 20:31:07 +0100

    All changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Providers from "../components/Providers";
+import "../styles/globals.css";
+
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Providers>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Providers>
+  );
 }
 
-export default MyApp
+export default MyApp;
